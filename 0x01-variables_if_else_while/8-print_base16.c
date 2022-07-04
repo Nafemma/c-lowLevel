d5403b36e5f8dacae434eb4d6999ea93923b76b8
Alinafe Mpofu <mpofuemmanuellah@gmail.com>
2022-07-04 15:53:38 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,15 @@ int main(void)
 int num;
 char letter;
 
-for (num = 0; num <= 10; num++)
-putchar((num % 10) + '0');
-
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
 for (letter = 'a'; letter <= 'f'; letter++)
+{
 putchar(letter);
+}
+putchar (10);
 
 return (0);
 }
