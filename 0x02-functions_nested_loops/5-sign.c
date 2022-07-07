2799059a3a19762efd4f118cad982f47cb8cc26a
Alinafe Mpofu <mpofuemmanuellah@gmail.com>
2022-07-07 06:31:50 -0700

    prints sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - function checks for a sign number
+ * @n: is the int used for the argument of the function
+ * Return: 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (0);
+}
+}
