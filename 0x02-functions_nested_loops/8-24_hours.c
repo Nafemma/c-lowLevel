cb519f84b22f53ffa308f86d626da11bf0572457
Alinafe Mpofu <mpofuemmanuellah@gmail.com>
2022-07-07 13:29:13 -0700

    calculates time for jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ while (hours <= 23)
 {
 while (minutes <= 59)
 {
-mins_remainder = minutes % 10;
+minutes_remainder = minutes % 10;
 hours_remainder = hours % 10;
 _putchar(hours / 10 + '0');
 _putchar(hours_remainder + '0');
