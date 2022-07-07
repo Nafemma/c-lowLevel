fbd428718e44e15fefe0d5b36fcf1b5e5a234e33
Alinafe Mpofu <mpofuemmanuellah@gmail.com>
2022-07-07 05:08:58 -0700

    lowercase alphbet followed by new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function prints abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_purchar(c);
+}
+_putchar('\n');
+}
