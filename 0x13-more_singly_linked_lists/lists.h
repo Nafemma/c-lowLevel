f06a5b8bab20b62b4fa0aad440e13d4b7e02daae
Nafemma <mpofuemmanuellah@gmail.com>
2022-08-16 14:09:29 -0700

    please work....

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,7 +9,6 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  *
  */
@@ -48,4 +47,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif_
+#endif
